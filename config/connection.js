59a82788b5537e63c3b90ef1e324f55a832848e7
Jandrei Timoteo <jandrei_tim@yahoo.com>
2024-01-08 15:22:08 -0800

    Changed the database in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,5 @@
 const { connect, connection } = require('mongoose');
 
-const connectionString = 'mongodb://127.0.0.1:27017/thoughtsDB';
-
-connect(connectionString);
+connect('mongodb://127.0.0.1:27017/socialNetwork');
 
 module.exports = connection;
