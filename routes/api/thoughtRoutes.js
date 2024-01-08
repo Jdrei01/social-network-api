7dc889429730f6dd1f87bee13ce580fd52c69711
Jandrei Timoteo <jandrei_tim@yahoo.com>
2024-01-08 15:50:08 -0800

    Added update thought, remove thought and create reaction

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,23 +1,20 @@
 const router = require('express').Router();
 const {
-  getThoughts,
-  getSingleThought,
-  createThought,
-  deleteThought,
-  addReaction,
-  removeReaction,
-} = require('../../controllers/thoughtController');
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    removeThought,
+    createReaction,
+    removeReaction
+  } = require('../../controllers/thoughtController');
 
-// /api/thoughts
 router.route('/').get(getThoughts).post(createThought);
 
-// /api/thoughts/:thoughtId
-router.route('/:thoughtId').get(getSingleThought).delete(deleteThought);
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(removeThought);
 
-// /api/thoughts/:thoughtId/reactions
-router.route('/:thoughtId/reactions').post(addReaction);
+router.route('/:thoughtId/reactions').post(createReaction);
 
-// /api/thoughts/:thoughtId/reactions/:reactionId
 router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
 
 module.exports = router;
